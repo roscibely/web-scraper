f326d0d6df9d3d7abd6a56f9843e291dc1c77d40
roscibely <rosanacibely@gmail.com>
2021-05-17 15:56:11 -0300

    Update exemple2.py

--- exemple2.py
+++ exemple2.py
@@ -1,6 +1,19 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
-# create instance of webdriver
-driver = webdriver.Chrome('C:\Program Files (x86)\chromedriver.exe')
-# Code to open google url
-driver.get('https://www.google.com')
+driver = webdriver.Chrome('C:\Program Files (x86)\chromedriver.exe') # create instance of webdriver
+driver.get('https://www.google.com')                                 # open google url
+keyword = 'AI'                                                       # set the keyword you want to search for
+searchBar = driver.find_element_by_name('q')                         # we find the search bar using it's name attribute value
+searchBar.send_keys(keyword)                                         # first we send our keyword to the search bar followed by the enter \n
+searchBar.send_keys('\n')
+time.sleep(5)
+try:
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "myDynamicElement"))
+    )
+finally:
+    driver.quit()
