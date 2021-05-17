a255224e76fe6edb53502be1496721277c37e031
roscibely <rosanacibely@gmail.com>
2021-05-17 15:42:53 -0300

    Create exemple2.py

---
+++ exemple2.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+
+# create instance of webdriver
+driver = webdriver.Chrome('C:\Program Files (x86)\chromedriver.exe')
+# Code to open google url
+driver.get('https://www.google.com')
